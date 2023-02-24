be6663f4dfefe169440deb3a5a2db1720e8fc4fe
Dan <danshelepin@gmail.com>
2023-02-24 14:23:27 +0300

    1423

---
+++ JS/Calculator.js
@@ -0,0 +1,13 @@
+let calc = {
+    name : "Calculator",
+    sum : function(a, b) { 
+        let c = a + b;
+        return c;
+    },
+    min : function(a, b) {
+        let c = a - b;
+        return c
+    }  
+}
+//calc.sum(100, 200)
+//calc.min(1000, 800)
