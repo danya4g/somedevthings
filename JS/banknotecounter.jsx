0867da06c94aae7c9ed902dfca36f680a682129b
Dan <danshelepin@gmail.com>
2023-02-25 15:39:09 +0300

    1539

---
+++ JS/banknotecounter.jsx
@@ -0,0 +1,9 @@
+let carPrice = 1700000;
+let myCash = 5000;
+let countBanknote = 1;
+
+while(myCash != carPrice) {
+    myCash += 5000;
+    countBanknote += 1;
+}
+console.log(countBanknote);
---
+++ JS/some whiles.js
@@ -0,0 +1,6 @@
+let x = 0;
+
+for(let i=1; i<=5; i+=1) {
+    x+=5;
+}
+console.log(x)
