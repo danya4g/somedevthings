04f97ba68e107f10216082c8a9edf4903aea6f6b
Dan <danshelepin@gmail.com>
2023-02-28 22:44:04 +0300

    2244

--- Python/PyProjects/folders.py
+++ Python/PyProjects/folders.py
@@ -6,15 +6,23 @@ from watchdog.events import FileSystemEventHandler
 class Handler(FileSystemEventHandler):
     def on_modified(self, event):
         for filename in os.listdir(folder_track):
-            file = folder_track + "/" + filename
-            new_path = folder_dest + "/" + filename
-            os.rename(file, new_path)
+            extension = filename.split(".")
+            if len(extension) > 1 and (
+                    extension[1].lower() == "jpg" or extension[1].lower() == "png" or extension[1].lower() == "svg"):
+                file = folder_track + "/" + filename
+                new_path = folder_dest + "/Photos/" + filename
+                os.rename(file, new_path)
+            elif len(extension) > 1 and extension[1].lower() == "mp4":
+                file = folder_track + "/" + filename
+                new_path = folder_dest + "/Videos/" + filename
+                os.rename(file, new_path)
 
-folder_track = 'Users/daniil/Downloads'
-folder_dest = 'Users/daniil'
+folder_track = '/Users/daniil/Downloads'
+folder_dest = '/Users/daniil/Images'
+
 handle = Handler()
 observer = Observer()
-observer.schedule(handle, folder_track, recursive=True)
+observer.schedule(handle, folder_track, recursive = True)
 observer.start()
 
 try:
